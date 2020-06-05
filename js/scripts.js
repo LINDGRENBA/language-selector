88c5a7f737bb60a4fce161c623c5d26a5c4b3a40
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-05 10:54:04 -0700

    Delete Poi from answer C, question #8 in index.html, add variables to scripts.js to select value of users answer.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,15 @@ $(document).ready(function() {
   $("#quiz-form").submit(function(event) {
     event.preventDefault();
 
-    
+    let selectedMeal = parseInt($("input:radio[name=meal]:checked").val());
+    let selectedLocation = parseInt($("input:radio[name=location]:checked").val());
+    let selectedOccupation = parseInt($("input:radio[name=occupation]:checked").val());
+    let selectedAnimal = parseInt($("input:radio[name=animal]:checked").val());
+    let selectedMusicGroup = parseInt($("input:radio[name=music-group]:checked").val());
+    let selectedSalary = parseInt($("input:radio[name=salary]:checked").val());
+    let selectedCelebrity = parseInt($("input:radio[name=celebrity]:checked").val());
+    let selectedActivity = parseInt($("input:radio[name=activity]:checked").val());
+
 
   });
 });
